37a3185e3f6b39043d363cb27feaf8d9f69b029f
BlockBusterBPL <beckettloose@me.com>
2023-04-14 13:29:12 -0400

    Adding base files and class structure

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,70 +4,214 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.inputs.LoggedPowerDistribution;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.hal.AllianceStationID;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.Mode;
+import frc.robot.Constants.RobotType;
+import frc.robot.lib.Alert;
+import frc.robot.lib.BatteryTracker;
+import frc.robot.lib.Alert.AlertType;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-
-  private RobotContainer m_robotContainer;
-
-  @Override
-  public void robotInit() {
-    m_robotContainer = new RobotContainer();
-  }
-
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+public class Robot extends LoggedRobot {
+    private static final String batteryNameFile = "/home/lvuser/battery-name.txt";
+    
+    private RobotContainer robotContainer;
+    private double autoStart;
+    private boolean autoMessagePrinted;
+    private boolean batteryNameWritten = false;
+    
+    private final Alert logNoFileAlert =
+    new Alert("No log path set for current robot. Data will NOT be logged.", AlertType.WARNING);
+    private final Alert logReceiverQueueAlert =
+    new Alert("Logging queue exceeded capacity, data will NOT be logged.", AlertType.ERROR);
+    private final Alert sameBatteryAlert =
+    new Alert("The battery has not been changed since the last match.", AlertType.WARNING);
+    
+    public Robot() {
+        super(Constants.loopPeriodSecs);
     }
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    
+    @Override
+    public void robotInit() {
+        Logger logger = Logger.getInstance();
+        
+        // Record metadata
+        logger.recordMetadata("Robot", Constants.getRobot().toString());
+        logger.recordMetadata("BatteryName", BatteryTracker.scanBattery(1.0));
+        logger.recordMetadata("TuningMode", Boolean.toString(Constants.tuningMode));
+        logger.recordMetadata("RuntimeType", getRuntimeType().toString());
+        logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        switch (BuildConstants.DIRTY) {
+            case 0:
+            logger.recordMetadata("GitDirty", "All changes committed");
+            break;
+            case 1:
+            logger.recordMetadata("GitDirty", "Uncomitted changes");
+            break;
+            default:
+            logger.recordMetadata("GitDirty", "Unknown");
+            break;
+        }
+        
+        // Set up data receivers & replay source
+        switch (Constants.getMode()) {
+            case REAL:
+            String folder = Constants.logFolders.get(Constants.getRobot());
+            if (folder != null) {
+                logger.addDataReceiver(new WPILOGWriter(folder));
+            } else {
+                logNoFileAlert.set(true);
+            }
+            logger.addDataReceiver(new NT4Publisher());
+            if (Constants.getRobot() == RobotType.ROBOT_2023C) {
+                LoggedPowerDistribution.getInstance(1, ModuleType.kRev);
+            }
+            break;
+            
+            case SIM:
+            logger.addDataReceiver(new NT4Publisher());
+            break;
+            
+            case REPLAY:
+            String path = LogFileUtil.findReplayLog();
+            logger.setReplaySource(new WPILOGReader(path));
+            logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(path, "_sim")));
+            break;
+        }
+        
+        // Start AdvantageKit logger
+        setUseTiming(Constants.getMode() != Mode.REPLAY);
+        logger.start();
+        
+        // Check for battery alert
+        if (Constants.getMode() == Mode.REAL
+        && !BatteryTracker.getName().equals(BatteryTracker.defaultName)) {
+            File file = new File(batteryNameFile);
+            if (file.exists()) {
+                // Read previous battery name
+                String previousBatteryName = "";
+                try {
+                    previousBatteryName =
+                    new String(Files.readAllBytes(Paths.get(batteryNameFile)), StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+                if (previousBatteryName.equals(BatteryTracker.getName())) {
+                    // Same battery, set alert
+                    sameBatteryAlert.set(true);
+                    //Leds.getInstance().sameBattery = true;
+                } else {
+                    // New battery, delete file
+                    file.delete();
+                }
+            }
+        }
+        
+        // Log active commands
+        Map<String, Integer> commandCounts = new HashMap<>();
+        BiConsumer<Command, Boolean> logCommandFunction =
+        (Command command, Boolean active) -> {
+            String name = command.getName();
+            int count = commandCounts.getOrDefault(name, 0) + (active ? 1 : -1);
+            commandCounts.put(name, count);
+            Logger.getInstance()
+            .recordOutput(
+            "CommandsUnique/" + name + "_" + Integer.toHexString(command.hashCode()), active);
+            Logger.getInstance().recordOutput("CommandsAll/" + name, count > 0);
+        };
+        CommandScheduler.getInstance()
+        .onCommandInitialize(
+        (Command command) -> {
+            logCommandFunction.accept(command, true);
+        });
+        CommandScheduler.getInstance()
+        .onCommandFinish(
+        (Command command) -> {
+            logCommandFunction.accept(command, false);
+        });
+        CommandScheduler.getInstance()
+        .onCommandInterrupt(
+        (Command command) -> {
+            logCommandFunction.accept(command, false);
+        });
+        
+        // Default to blue alliance in sim
+        if (Constants.getMode() == Mode.SIM) {
+            DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
+        }
+        
+        robotContainer = new RobotContainer();
     }
-  }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
+    
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+    
+    @Override
+    public void disabledInit() {}
+    
+    @Override
+    public void disabledPeriodic() {}
+    
+    @Override
+    public void disabledExit() {}
+    
+    @Override
+    public void autonomousInit() {
+        
+    }
+    
+    @Override
+    public void autonomousPeriodic() {}
+    
+    @Override
+    public void autonomousExit() {}
+    
+    @Override
+    public void teleopInit() {
+        
+    }
+    
+    @Override
+    public void teleopPeriodic() {}
+    
+    @Override
+    public void teleopExit() {}
+    
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+    
+    @Override
+    public void testPeriodic() {}
+    
+    @Override
+    public void testExit() {}
 }
